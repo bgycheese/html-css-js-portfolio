f19f6e7d3aee776d59aecd4742b50392c9f5c5d1
Kamran Abbasli <kamran.abbasli@virtuozzo.com>
2026-03-20 16:09:13 +0100

    Kamran portfolio

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    // inbuilt system that taragets tthe element on the webpage and using the element on the page.
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    // add a class list, using the inbuilt function toggle and adding the open class.
+    icon.classList.toggle("open");
+}
+
+function iconClick(){
+    window.location.reload()    
+}
+
